8847fd3c1cac50ff1277a010ec2aadc05a9b7082
david99cartagena <32483852+david99cartagena@users.noreply.github.com>
2025-09-20 19:07:43 -0500

    proyecto completo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { authGuard } from './services/auth.guard';
+import { HomeComponent } from './components/home/home.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { CallbackComponent } from './components/callback/callback.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  {
+    path: 'protegida',
+    component: ProtegidaComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'callback', component: CallbackComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
